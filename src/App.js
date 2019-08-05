2ed8e6f2e5045e9b7fcd360e06bf3436e152b191
snoopy2199 <snoopy2199@gmail.com>
2019-08-05 21:23:10 +0800

    Listen to the beforeinstallprompt event

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [promptEvent, setPromptEvent] = useState(null);
+
+  const handlePromotion = () => {
+    promptEvent.prompt();
+    promptEvent.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        setPromptEvent(null);
+      });
+  }
+
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      setPromptEvent(e);
+      console.log('beforeinstallprompt');
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +41,7 @@ function App() {
           Learn React
         </a>
       </header>
+      {promptEvent && <button onClick={handlePromotion}>install</button>}
     </div>
   );
 }
