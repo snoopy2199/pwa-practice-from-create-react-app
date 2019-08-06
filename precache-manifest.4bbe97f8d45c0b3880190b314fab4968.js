a7172cf1b5412b97f69265e351c2b5e7a4e83055
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-06 00:35:33 +0000

    Deploy snoopy2199/pwa-practice-from-create-react-app to github.com/snoopy2199/pwa-practice-from-create-react-app.git:gh-pages

---
+++ precache-manifest.4bbe97f8d45c0b3880190b314fab4968.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ec2e8956e0769adf006097160e332e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "137d707d3db9670b67d4",
+    "url": "./static/css/main.8da8f3ee.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "./static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "137d707d3db9670b67d4",
+    "url": "./static/js/main.afbe37e1.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
